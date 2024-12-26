5b192abbec5567797d8ad400b1e6b70dc7cf36ec
Vladislav2397 <valdiszz53@gmail.com>
2024-12-26 16:47:26 +0300

    Initialize

---
+++ script.js
@@ -0,0 +1,28 @@
+const viewportOffsetTop = document.querySelector('#viewport-offsetTop')
+const viewportPageTop = document.querySelector('#viewport-pageTop')
+const viewportWidth = document.querySelector('#viewport-width')
+const viewportHeight = document.querySelector('#viewport-height')
+
+const screenWidth = document.querySelector('#screen-width')
+const screenHeight = document.querySelector('#screen-height')
+
+function onResize() {
+    const vp = window.visualViewport
+
+    viewportOffsetTop.innerHTML = `offsetTop: ${vp.offsetTop}`
+    viewportPageTop.innerHTML = `pageTop: ${vp.pageTop}`
+    viewportWidth.innerHTML = `width: ${vp.width}`
+    viewportHeight.innerHTML = `height: ${vp.height}`
+}
+
+function onScreen() {
+    const data = window.screen
+
+    screenWidth.innerHTML = `width: ${data.width}`
+    screenHeight.innerHTML = `height: ${data.height}`
+}
+
+window.visualViewport.addEventListener('resize', onResize)
+onResize()
+window.visualViewport.addEventListener('resize', onScreen)
+onScreen()
